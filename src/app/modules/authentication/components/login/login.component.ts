68591c8bad2525368bf85f8bb5be3f00e97eb25e
Michael Johnson <mcangelo200@gmail.com>
2020-01-23 23:43:57 +0000

    --made the login form work

--- src/app/modules/authentication/components/login/login.component.ts
+++ src/app/modules/authentication/components/login/login.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
+	public enableSignUpForm: boolean;
+	public enableSignInForm: boolean;
   constructor() { }
 
   ngOnInit() {
+  }
+
+  public switchForm(enableForm: string): void {  	
+
+  	if(enableForm === 'signIn'){
+  		this.enableSignUpForm = false;
+  		this.enableSignInForm = true;
+  	}
+
+  	if(enableForm === 'signUp'){
+  		this.enableSignUpForm = true;
+  		this.enableSignInForm = false;
+  	}
   }
 
 }
