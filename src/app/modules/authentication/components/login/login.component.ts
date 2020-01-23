c75c6df188c2d21b6a8aa54b3cf09b2963eef6f6
Michael Johnson <mcangelo200@gmail.com>
2020-01-23 23:47:45 +0000

    --did some minor refactoring

--- src/app/modules/authentication/components/login/login.component.ts
+++ src/app/modules/authentication/components/login/login.component.ts
@@ -1,30 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+	selector: 'app-login',
+	templateUrl: './login.component.html',
+	styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
 
 	public enableSignUpForm: boolean;
-	public enableSignInForm: boolean;
-  constructor() { }
+	constructor() { }
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
-  public switchForm(enableForm: string): void {  	
+	public switchForm(enableForm: string): void {  	
 
-  	if(enableForm === 'signIn'){
-  		this.enableSignUpForm = false;
-  		this.enableSignInForm = true;
-  	}
+		if(enableForm === 'signIn'){
+			this.enableSignUpForm = false;
+		}
 
-  	if(enableForm === 'signUp'){
-  		this.enableSignUpForm = true;
-  		this.enableSignInForm = false;
-  	}
-  }
+		if(enableForm === 'signUp'){
+			this.enableSignUpForm = true;
+		}
+	}
 
 }
